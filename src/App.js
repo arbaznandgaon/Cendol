15c74107da89c9fd4c541b81d7c37eddcb2ec3e5
Arbaz Nandgaon <arbaznandgaon@gmail.com>
2023-06-20 12:50:18 +0530

    001

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import Home from "./First/Home";
 import Home2 from "./Second/Home2";
 import B from "./Third/B";
+import Page1 from "./Tech/Page1";
 
 function App() {
   return (
     <div className="App">
       {/* <Home/> */}
       {/* <Home2/> */} 
-      <B />
+      {/* <B /> */}
+      <Page1/>
     </div>
   );
 }
---
+++ src/Tech/Page1.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+function Page1() {
+
+var obj ={
+    name: "Arbaz" ,
+    Place: "Athani"
+}
+
+obj.Num="000"
+
+
+console.log(obj)
+
+
+
+  return (
+    <div>
+
+
+
+    </div>
+  )
+}
+
+export default Page1
